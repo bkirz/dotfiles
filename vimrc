a82f36c0da41986a56c4ccdd6f02cfec595693fc
Ben Kirzhner <bkirz@cs.umd.edu>
2011-07-30 09:59:58 -0700

    Initial commit with vimrc/zsh.init

---
+++ vimrc
@@ -0,0 +1,16 @@
+" The littlest vimrc that could
+
+set nocompatible
+
+" Enable pathogen
+call pathogen#runtime_append_all_bundles()
+
+" Enable syntax highlighting
+syntax on
+filetype plugin on
+
+" Indentation preferences
+set expandtab
+set tabstop=2
+set shiftwidth=2
+set smartindent
---
+++ zsh/init.zsh
@@ -0,0 +1,25 @@
+# load rvm stuffs
+[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
+NODE_PATH=/usr/local/lib/node
+
+autoload -U compinit promptinit colors
+promptinit
+colors
+ 
+alias ocaml="rlwrap ocaml"
+alias coqtop="rlwrap coqtop"
+
+user_color="%(!.%{${fg[red]}%}.%{${fg[green]}%})"
+
+export PATH=$HOME/go/bin:$HOME/.bin:/opt/local/bin:$PATH:/usr/local/mysql/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/local/lib
+
+#export PROMPT="${user_color}[%{${reset_color}%}%m:%~${user_color}]%#%{${reset_color}%} "
+#export RPROMPT="${user_color}(%{${reset_color}%}%h - %*${user_color}%)%{${reset_color}%}"
+export EDITOR=vim
+
+setopt promptsubst
+autoload -U promptinit
+promptinit
+prompt bkirz
