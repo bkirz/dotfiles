ff85aa03d03372d07b9a384477037b1090c96f9c
Ben Kirzhner <ben@bkirz.com>
2011-12-27 11:31:35 -0500

    Remove outdated stuff in init.zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,18 +1,13 @@
 # load rvm stuffs
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
-NODE_PATH=/usr/local/lib/node
-
 autoload -U compinit promptinit colors
 promptinit
 colors
- 
-alias ocaml="rlwrap ocaml"
-alias coqtop="rlwrap coqtop"
 
 user_color="%(!.%{${fg[red]}%}.%{${fg[green]}%})"
 
-export PATH=$HOME/go/bin:$HOME/.bin:/opt/local/bin:$PATH:/usr/local/mysql/bin
+export PATH=$HOME/.bin:/usr/local/bin:/usr/local/sbin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/local/lib
 
 export PROMPT="${user_color}[%{${reset_color}%}%m:%~${user_color}]%#%{${reset_color}%} "
