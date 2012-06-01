da959a888ac359f4d870fc77dbf6a0796842b6ba
Ben Kirzhner <ben@bkirz.com>
2012-06-01 01:01:59 -0700

    Move Rakefile into vanilla ruby script

--- install.rb
+++ install.rb
@@ -1,13 +1,11 @@
-# Rakefile adapted from ryanb's (https://github.com/ryanb/dotfiles/)
 require 'date'
-require 'rake'
+require 'fileutils'
 require 'pathname'
 
 HOME = Pathname.new(ENV['HOME'])
-SKIP_FILES = %w[.git .gitignore .gitmodules Rakefile].map{ |s| Pathname.new(s) }
+SKIP_FILES = %w[.git .gitignore .gitmodules install.rb].map{ |s| Pathname.new(s) }
 
-desc "install the dot files into user's home directory"
-task :install do
+def run
   safe_dir_path = HOME + DateTime.now.strftime('old-dotfiles-%Y%m%d-%H%M%S')
   paths_to_install = Pathname.getwd.children.reject { |c| SKIP_FILES.include?(c.basename) }
   paths_to_install.each { |path| install_path(path, safe_dir_path) }
@@ -17,6 +15,7 @@ def install_path(path, safe_dir_path)
   destination = HOME + ".#{path.basename}"
   backup_to_safe_dir(destination, safe_dir_path) if destination.exist?
   destination.make_symlink(path)
+  puts "Installed #{destination}"
 rescue => exn
   puts "Exception while attempting to install #{path}: #{exn}.  Moving on."
 end
@@ -25,4 +24,7 @@ def backup_to_safe_dir(file, safe_dir_path)
   safe_dir_path.mkdir unless safe_dir_path.exist?
   backup_path = safe_dir_path + file.basename
   FileUtils.mv(file, backup_path)
+  puts "Backed up #{file} to #{backup_path}"
 end
+
+run
